6f21016909fa0fdc9bfaf01051dc6636bacfecd1
Alexey Boltava <goodmoveab@gmail.com>
2018-02-14 22:18:41 +0700

    Add IGridController interface

---
+++ Life/src/ru/nsu/fit/g15201/boltava/model/graphics/IGridController.scala
@@ -0,0 +1,24 @@
+package ru.nsu.fit.g15201.boltava.model.graphics
+
+import ru.nsu.fit.g15201.boltava.model.logic.Cell
+
+
+/**
+  * This class is intended to control various grid systems.
+  *
+  * 1. Creates arbitrary cell space with its own coordinate system
+  * @tparam CellType
+  */
+trait IGridController[CellType <: Cell] {
+
+  def generateGrid(width: Int, height: Int): Array[CellType]
+
+  /**
+    * Get cartesian coordinates for cell with coordinates `point`,
+    * given in terms of this space
+    * @param point point to convert to Cartesian
+    * @return new point, which represents the passed point in Cartesian coordinates
+    */
+  def getCenterPixelCoords(point: Point): Point
+
+}
