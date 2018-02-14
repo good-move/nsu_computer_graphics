26250ced8afa85f78a53356597cd64346e813465
Alexey Boltava <goodmoveab@gmail.com>
2018-02-14 22:17:44 +0700

    Implement BresenhamLineCreator class

---
+++ Life/src/ru/nsu/fit/g15201/boltava/model/graphics/BresenhamLineCreator.scala
@@ -0,0 +1,41 @@
+package ru.nsu.fit.g15201.boltava.model.graphics
+
+import scala.collection.mutable.ArrayBuffer
+
+object BresenhamLineCreator extends LineCreator {
+
+  override def getLinePoints(from: Point, to: Point): Array[Point] = {
+    val dx: Int = Math.abs(from.x - to.x)
+    val dy: Int = Math.abs(from.y - to.y)
+    var linePoints = ArrayBuffer[Point]()
+
+    val yStep = if (from.y <= to.y) 1 else -1
+    val xStep = if (from.x <= to.x) 1 else -1
+    var error: Double = 0
+    var x = from.x
+    var y = from.y
+
+    val pointsToAdd: Int = Math.max(dx, dy) + 1
+
+    for (_ <- 1 to pointsToAdd) {
+      linePoints.append((x, y))
+      if (dx >= dy) {
+        error += dy
+        if (2 * error >= 1) {
+          y += yStep
+          error -= dx
+        }
+        x += xStep
+      } else {
+        error += dx
+        if (2 * error >= 1) {
+          x += xStep
+          error -= dy
+        }
+        y += yStep
+      }
+    }
+
+    linePoints.toArray
+  }
+}
