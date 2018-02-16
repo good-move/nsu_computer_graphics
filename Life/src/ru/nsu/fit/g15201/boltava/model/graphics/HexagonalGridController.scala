84f98c70a3c502508ab6f183deca810eccb75c05
Alexey Boltava <goodmoveab@gmail.com>
2018-02-16 23:17:07 +0700

    Make use of new DoublePoint class in HexGridController

--- Life/src/ru/nsu/fit/g15201/boltava/model/graphics/HexagonalGridController.scala
+++ Life/src/ru/nsu/fit/g15201/boltava/model/graphics/HexagonalGridController.scala
@@ -6,15 +6,15 @@ import scala.collection.mutable.ArrayBuffer
 
 class HexagonalGridController(private val hexSideSize: Int) extends IGridController[Hexagon] {
 
-  private val _size: Double = hexSideSize.toDouble
+  private val _size: Double = hexSideSize
   private val R: Double = _size
-  private val r: Double = Math.sqrt(3.0)/2.0 * _size
-  private val xStep: Double = Math.sqrt(3.0)*_size
-  private val yStep: Double = 3.0*_size/2.0
-  private val bias = (r, R)
+  private val r: Double = Math.sqrt(3)* _size/2.0
+  private val xStep: Double = 2.0*r
+  private val yStep: Double = 1.5*R
+  private val bias: (Double, Double) = (r, R)
 
-  private val ROTATION_ANGLE = 60
-  private val OFFSET_ANGLE = 30
+  private val ROTATION_ANGLE = 60.0
+  private val OFFSET_ANGLE = 30.0
 
   def getSideSize: Int = hexSideSize
 
@@ -24,33 +24,32 @@ class HexagonalGridController(private val hexSideSize: Int) extends IGridControl
     for (x <- 0 until height) {
       grid(x) = new Array[Hexagon](width)
       for (y <- 0 until width) {
-        val center = getCenterPixelCoords(x,y)
+        val center = getCellCenter(x,y)
         val vertices = getVerticesForHexCenter(center)
-        grid(x)(y) = Hexagon(center, vertices)
+        grid(x)(y) = Hexagon((center.x.toInt, center.y.toInt), vertices)
       }
     }
 
     grid
   }
 
-  override def getCenterPixelCoords(point: Point): Point = {
-    (
-      Math.round(bias._1 + point.y.toDouble * xStep + r * (point.x & 1)).toInt,
-      Math.round(bias._2 + point.x.toDouble * yStep).toInt
-    )
+  override def getCellCenter(point: Point): DoublePoint = {
+    val x =  bias._1 + point.y.toDouble * xStep + r * (point.x & 1).toDouble
+    val y =  bias._2 + point.x.toDouble * yStep
+    (x, y)
   }
 
-  def getVerticesForHex(point: Point): Array[Point] = {
-    getVerticesForHexCenter(getCenterPixelCoords(point))
+  private def getVerticesForHex(point: Point): Array[Point] = {
+    getVerticesForHexCenter(getCellCenter(point))
   }
 
-  def getVerticesForHexCenter(center: Point): Array[Point] = {
+  private def getVerticesForHexCenter(center: DoublePoint): Array[Point] = {
     var vertices = ArrayBuffer[Point]()
 
-    var angle_deg = OFFSET_ANGLE
-    for (i <- 0 to 6) {
+    var angle_deg: Double = OFFSET_ANGLE
+    for (_ <- 0 to 6) {
       angle_deg += ROTATION_ANGLE
-      val angle_rad = Math.PI / 180 * angle_deg
+      val angle_rad = angle_deg/180.0  * Math.PI
       vertices.append((
         Math.round(center.x + _size * Math.cos(angle_rad)).toInt,
         Math.round(center.y + _size * Math.sin(angle_rad)).toInt
@@ -58,6 +57,12 @@ class HexagonalGridController(private val hexSideSize: Int) extends IGridControl
     }
 
     vertices.toArray
+  }
+
+  def createHex(point: Point): Hexagon = {
+    val center = getCellCenter(point)
+    val vertices = getVerticesForHex(point)
+    Hexagon(new Point(center.x, center.y), vertices)
   }
 
 }
