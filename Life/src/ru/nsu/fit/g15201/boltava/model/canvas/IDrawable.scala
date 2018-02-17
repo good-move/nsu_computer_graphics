b166dea0a4c2c78ebe7d40051c73c164b4017f51
Alexey Boltava <goodmoveab@gmail.com>
2018-02-17 23:09:27 +0700

    Introduce IDrawable interface

---
+++ Life/src/ru/nsu/fit/g15201/boltava/model/canvas/IDrawable.scala
@@ -0,0 +1,11 @@
+package ru.nsu.fit.g15201.boltava.model.canvas
+
+import javafx.scene.paint.Color
+
+trait IDrawable {
+  def getHeight: Double
+  def getWidth: Double
+  def setColor(point: Point, color: Color): Unit
+  def getColor(point: Point): Color
+  def draw(points: Array[Point], color: Color): Unit
+}
