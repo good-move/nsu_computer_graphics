c900560ca33928530623df665b5ec15a0e9b7784
Alexey Boltava <goodmoveab@gmail.com>
2018-02-17 23:34:09 +0700

    Add Main (launcher) class

---
+++ Life/src/ru/nsu/fit/g15201/boltava/Main.scala
@@ -0,0 +1,11 @@
+package ru.nsu.fit.g15201.boltava
+
+import javafx.application.Application
+
+object Main {
+
+  def main(args: Array[String]): Unit = {
+    Application.launch(classOf[LifeGame], args: _*)
+  }
+
+}
