286920d3561ecd64ac77d407f53fdced84cac510
Alexey Boltava <goodmoveab@gmail.com>
2018-02-17 23:46:42 +0700

    Replace Stack with more efficient ListBuffer in ScanLineFilter

--- Life/src/ru/nsu/fit/g15201/boltava/model/graphics/ScanLineFiller.scala
+++ Life/src/ru/nsu/fit/g15201/boltava/model/graphics/ScanLineFiller.scala
@@ -5,7 +5,7 @@ import javafx.scene.paint.Color
 import ru.nsu.fit.g15201.boltava.model.canvas.{IDrawable, Point}
 import ru.nsu.fit.g15201.boltava.model.logic.Hexagon
 
-import scala.collection.mutable
+import scala.collection.mutable.{ListBuffer}
 
 class ScanLineFiller extends IColorFiller[Hexagon] {
 
@@ -14,11 +14,11 @@ class ScanLineFiller extends IColorFiller[Hexagon] {
 
     if (oldColor.equals(newColor)) return
 
-    val stack = new mutable.Stack[Point]()
-    stack.push(hex.center)
+    var stack = new ListBuffer[Point]()
+    stack.append(hex.center)
 
     while (stack.nonEmpty) {
-      val p = stack.pop()
+      val p = stack.remove(stack.size-1)
       var y = p.y
       while (y >= 0 && drawable.getColor(p.x, y) == oldColor) {
         y -= 1
@@ -29,13 +29,13 @@ class ScanLineFiller extends IColorFiller[Hexagon] {
       while (y < drawable.getHeight && drawable.getColor(p.x, y) == oldColor) {
         drawable.setColor((p.x, y), newColor)
         if (!spanLeft && p.x-1 >= 0 && drawable.getColor(p.x-1, y) == oldColor) {
-          stack.push((p.x - 1, y))
+          stack.append((p.x - 1, y))
           spanLeft = false
         } else if (spanLeft && p.x-1 >= 0 && drawable.getColor(p.x-1, y) != oldColor) {
           spanLeft = false
         }
         if (!spanRight && p.x < drawable.getWidth-1 && drawable.getColor(p.x+1, y) == oldColor) {
-          stack.push((p.x + 1, y))
+          stack.append((p.x + 1, y))
           spanRight = false
         } else if (spanRight && p.x+1 < drawable.getWidth && drawable.getColor(p.x+1, y) != oldColor) {
           spanRight = false
