d5241e71d900ab29f1d15a996b4b237c4bbaa2e7
Alexey Boltava <goodmoveab@gmail.com>
2018-02-18 12:50:25 +0700

    Refactor Main class

--- Life/src/ru/nsu/fit/g15201/boltava/Main.scala
+++ Life/src/ru/nsu/fit/g15201/boltava/Main.scala
@@ -2,6 +2,8 @@ package ru.nsu.fit.g15201.boltava
 
 import javafx.application.Application
 
+import ru.nsu.fit.g15201.boltava.view.LifeGame
+
 object Main {
 
   def main(args: Array[String]): Unit = {
