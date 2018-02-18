b90b02189b8a190462c53c397f58a0c396d3fb8a
Alexey Boltava <goodmoveab@gmail.com>
2018-02-18 12:39:29 +0700

    Make GameController implement cell data interfaces

--- Life/src/ru/nsu/fit/g15201/boltava/model/logic/GameController.scala
+++ Life/src/ru/nsu/fit/g15201/boltava/model/logic/GameController.scala
@@ -1,15 +1,19 @@
-package ru.nsu.fit.g15201.boltava.model
+package ru.nsu.fit.g15201.boltava.model.logic
 
 import ru.nsu.fit.g15201.boltava.model.canvas.IGridController
 import ru.nsu.fit.g15201.boltava.model.canvas.geometry.Polygon
-import ru.nsu.fit.g15201.boltava.model.logic.Cell
+import ru.nsu.fit.g15201.boltava.view.ICellStateObserver
+
+import scala.collection.mutable
 
 class GameController[T <: Cell with Polygon](private val fieldWidth: Int = 10,
                                    private val fieldHeight: Int = 10,
-                                   private val gridController: IGridController[T]) {
+                                   private val gridController: IGridController[T]) extends ICellClickListener with ICellStateProvider {
 
   private var cellGrid: Array[Array[T]] = _
   private var cellSelectionMode = CellSelectionMode.TOGGLE
+
+  private var cellStateObservers = new mutable.HashSet[ICellStateObserver]()
 
   { // constructor code
     generateGrid()
@@ -31,6 +35,31 @@ class GameController[T <: Cell with Polygon](private val fieldWidth: Int = 10,
     cellGrid = gridController.generateGrid(fieldWidth, fieldHeight)
   }
 
+  override def onCellClicked(cell: Cell with Polygon): Unit = {
+    val oldState = cell.getState
+    if (cellSelectionMode == CellSelectionMode.TOGGLE) {
+      if (cell.getState == cell.State.ALIVE) {
+        cell.setState(cell.State.DEAD)
+      }  else {
+        cell.setState(cell.State.ALIVE)
+      }
+    } else {
+      cell.setState(cell.State.ALIVE)
+    }
+
+    if (cell.getState != oldState) {
+      cellStateObservers.foreach(o => o.onCellStateChange(cell))
+    }
+
+  }
+
+  override def subscribe(cellStateObserver: ICellStateObserver): Unit = {
+    cellStateObservers.add(cellStateObserver)
+  }
+
+  override def unsubscribe(cellStateObserver: ICellStateObserver): Unit = {
+    cellStateObservers.remove(cellStateObserver)
+  }
 }
 
 object CellSelectionMode extends Enumeration {
