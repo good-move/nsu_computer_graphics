79a4d341e1062f4e681f3e732bcc8f94af7581f9
Alexey Boltava <goodmoveab@gmail.com>
2018-02-18 12:37:02 +0700

    Make HexagonCell extend Polygon trait

--- Life/src/ru/nsu/fit/g15201/boltava/model/logic/HexagonCell.scala
+++ Life/src/ru/nsu/fit/g15201/boltava/model/logic/HexagonCell.scala
@@ -1,18 +1,20 @@
 package ru.nsu.fit.g15201.boltava.model.logic
 
-import ru.nsu.fit.g15201.boltava.model.canvas.geometry.{Hexagon, Point}
+import ru.nsu.fit.g15201.boltava.model.canvas.geometry.{Point, Polygon}
 
 class HexagonCell(private val center: Point,
-                  private val vertices: Array[Point]) extends Hexagon(center, vertices) with Cell {
+                  private val vertices: Array[Point]) extends Cell with Polygon {
 
   var state: State.Value = State.DEAD
 
-  override def getState: State.State = {
-    state
-  }
+  override def getState: State.State = state
 
   override def setState(state: State.State): Unit = {
     this.state = state
   }
+
+  override def getCenter: Point = center
+
+  override def getVertices: Array[Point] = vertices
 
 }
