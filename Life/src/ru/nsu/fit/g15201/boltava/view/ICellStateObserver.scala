bbf7cc13078702f5d25b383de5b76e6193d91a7d
Alexey Boltava <goodmoveab@gmail.com>
2018-02-19 20:21:51 +0700

    Remove Polygon trait from ICellStateObserver

--- Life/src/ru/nsu/fit/g15201/boltava/view/ICellStateObserver.scala
+++ Life/src/ru/nsu/fit/g15201/boltava/view/ICellStateObserver.scala
@@ -1,8 +1,8 @@
 package ru.nsu.fit.g15201.boltava.view
 
-import ru.nsu.fit.g15201.boltava.model.canvas.geometry.Polygon
 import ru.nsu.fit.g15201.boltava.model.logic.Cell
 
 trait ICellStateObserver {
-  def onCellStateChange(cell: Cell with Polygon)
+  def onCellStateChange(cell: Cell)
+  def onCellsStateChange(cell: Array[Array[Cell]])
 }
