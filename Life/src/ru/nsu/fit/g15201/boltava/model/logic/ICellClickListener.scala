469920174f7025684a44eebe19558af922c5cb0d
Alexey Boltava <goodmoveab@gmail.com>
2018-02-19 20:21:32 +0700

    Remove Polygon trait from ICellClick listener

--- Life/src/ru/nsu/fit/g15201/boltava/model/logic/ICellClickListener.scala
+++ Life/src/ru/nsu/fit/g15201/boltava/model/logic/ICellClickListener.scala
@@ -1,7 +1,5 @@
 package ru.nsu.fit.g15201.boltava.model.logic
 
-import ru.nsu.fit.g15201.boltava.model.canvas.geometry.Polygon
-
 trait ICellClickListener {
-  def onCellClicked(cell: Cell with Polygon)
+  def onCellClicked(cell: Cell)
 }
