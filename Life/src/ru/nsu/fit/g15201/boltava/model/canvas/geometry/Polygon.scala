01e0060800f696d11c0e2c6a4e34942caf12c951
Alexey Boltava <goodmoveab@gmail.com>
2018-02-19 20:17:14 +0700

    Move Polygon semantics to Cell interface and remove Polygon trait

--- Life/src/ru/nsu/fit/g15201/boltava/model/canvas/geometry/Polygon.scala
+++
@@ -1,6 +0,0 @@
-package ru.nsu.fit.g15201.boltava.model.canvas.geometry
-
-trait Polygon {
-  def getCenter: Point
-  def getVertices: Array[Point]
-}
--- Life/src/ru/nsu/fit/g15201/boltava/model/logic/Cell.scala
+++ Life/src/ru/nsu/fit/g15201/boltava/model/logic/Cell.scala
@@ -1,13 +1,18 @@
 package ru.nsu.fit.g15201.boltava.model.logic
 
+import ru.nsu.fit.g15201.boltava.model.canvas.geometry.Point
+
 trait Cell {
+
+  def getCenter: Point
+  def getVertices: Array[Point]
 
   def getState: State.State
   def setState(state: State.State): Unit
 
-  object State extends Enumeration {
-    type State = Value
-    val ALIVE, DEAD = Value
-  }
+}
 
+object State extends Enumeration {
+  type State = Value
+  val ALIVE, DEAD = Value
 }
