87b81e30e7ccec946c713afc9c840d0836351d23
Alexey Boltava <goodmoveab@gmail.com>
2018-02-20 11:36:49 +0700

    Unite Main and LifeGame classes

---
+++ Life/src/ru/nsu/fit/g15201/boltava/LifeGame.scala
@@ -0,0 +1,29 @@
+package ru.nsu.fit.g15201.boltava
+
+import javafx.application.Application
+import javafx.fxml.FXMLLoader
+import javafx.scene.paint.Color
+import javafx.scene.{Parent, Scene}
+import javafx.stage.Stage
+
+
+object LifeGame {
+
+  def main(args: Array[String]): Unit = {
+    Application.launch(classOf[LifeGame], args: _*)
+  }
+
+}
+
+class LifeGame extends Application {
+
+  private val windowTitle = "Conway Game Of Life"
+
+  override def start(primaryStage: Stage): Unit = {
+    val root: Parent = FXMLLoader.load(getClass.getResource("form.fxml"))
+    primaryStage.setTitle(windowTitle)
+    primaryStage.setScene(new Scene(root, 800, 500, Color.WHITE))
+    primaryStage.show()
+  }
+
+}
