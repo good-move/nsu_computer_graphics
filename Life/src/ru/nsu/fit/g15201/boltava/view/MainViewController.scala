ade8649c0709dfaefee5d155a2fdf21044241781
Alexey Boltava <goodmoveab@gmail.com>
2018-02-21 13:00:58 +0700

    Use new fxml elements in main view controller

--- Life/src/ru/nsu/fit/g15201/boltava/view/MainViewController.scala
+++ Life/src/ru/nsu/fit/g15201/boltava/view/MainViewController.scala
@@ -2,10 +2,11 @@ package ru.nsu.fit.g15201.boltava.view
 
 import javafx.application.Platform
 import javafx.fxml.FXML
-import javafx.scene.control.{Alert, ToolBar}
+import javafx.scene.control.ScrollPane.ScrollBarPolicy
+import javafx.scene.control.{ScrollPane, ToolBar}
 import javafx.scene.image.{ImageView, WritableImage}
 import javafx.scene.input.MouseEvent
-import javafx.scene.layout.HBox
+import javafx.scene.layout._
 import javafx.scene.paint.Color
 import javafx.stage.Window
 
@@ -24,22 +25,22 @@ class MainViewController extends ICellStateObserver {
   private val ALIVE_CELL_COLOR = Color.GRAY
   private val DEAD_CELL_COLOR = Color.WHITE
 
-  private val GRID_WIDTH =20
-  private val GRID_HEIGHT = 20
-  private val CELL_SIDE_SIZE = 30
+  private val GRID_WIDTH = 40
+  private val GRID_HEIGHT = 40
+  private val CELL_SIDE_SIZE = 15
 
+  @FXML private var root: VBox = _
   @FXML private var toolbar: ToolBar = _
   @FXML private var gameFieldImageView: ImageView = _
-  @FXML private var gameFieldWrapper: HBox = _
+  @FXML private var scrollPane: ScrollPane = _
 
   private var window: Window = _
+  private var writableImage: WritableImage = _
 
   // ************************* Controller initialization *************************
 
-
   @FXML
   private def initialize(): Unit = {
-    toolbar.getScene
     initializeGrid(GRID_WIDTH, GRID_HEIGHT)
 
     setEventHandlers()
@@ -48,19 +49,20 @@ class MainViewController extends ICellStateObserver {
   }
 
   private def setEventHandlers(): Unit = {
-    gameFieldWrapper.setOnMouseClicked((event: MouseEvent) => {
+    scrollPane.setOnMouseClicked((event: MouseEvent) => {
       onFieldDragOrClick((event.getX, event.getY))
     })
 
-    gameFieldWrapper.setOnDragDetected((event: MouseEvent) => {
+    scrollPane.setOnDragDetected((event: MouseEvent) => {
       gameFieldImageView.startFullDrag()
       event.consume()
     })
 
-    gameFieldWrapper.setOnMouseDragOver((event: MouseEvent) => {
+    scrollPane.setOnMouseDragOver((event: MouseEvent) => {
       onFieldDragOrClick((event.getX, event.getY))
     })
 
+    VBox.setVgrow(scrollPane, Priority.ALWAYS)
   }
 
   // ************************* FXML events *************************
@@ -69,6 +71,7 @@ class MainViewController extends ICellStateObserver {
   protected def onPlay(event: MouseEvent): Unit = {
     println("onPlay")
     window = toolbar.getScene.getWindow
+
     if (!gameController.isGameModelSet) {
       AlertHelper.showError(
         window,
@@ -130,29 +133,29 @@ class MainViewController extends ICellStateObserver {
 
   private def drawCellGrid(): Unit = {
     val (width, height) = gridController.getCartesianFieldSize(GRID_WIDTH, GRID_HEIGHT)
-    val image = new WritableImage(width.ceil.toInt, height.ceil.toInt)
-    gameFieldImageView.setImage(image)
-    drawable = new ImageDrawable(image)
-    fillWhiteBackground(drawable)
+    writableImage= new WritableImage(width.ceil.toInt, height.ceil.toInt)
+    gameFieldImageView.setImage(writableImage)
+    drawable = new ImageDrawable(writableImage)
 
     Platform.runLater(() => {
       gameController.getCells.foreach(_.foreach(cell => drawCell(drawable, cell)))
     })
+
   }
 
   private def onFieldDragOrClick(point: DoublePoint): Unit = {
-    val hexCoords = gridController.getCellByPoint(point)
+    val cellCoords = gridController.getCellByPoint(point)
     val cellGrid = gameController.getCells
-    println(s"$point -> $hexCoords")
-    if (hexCoords.x < 0 || hexCoords.y < 0 ||
-      hexCoords.x >= cellGrid.length || hexCoords.y >= cellGrid(0).length) return
+//    println(s"$point -> $cellCoords")
+    if (cellCoords.x < 0 || cellCoords.y < 0 ||
+      cellCoords.x >= cellGrid.length || cellCoords.y >= cellGrid(0).length) return
 
-    val hexagon = gameController.getCells(hexCoords.x)(hexCoords.y)
-    gameController.onCellClicked(hexagon)
+    val cell = gameController.getCells(cellCoords.x)(cellCoords.y)
+    gameController.onCellClicked(cell)
   }
 
   private def fillCell(cell: Cell, color: Color): Unit = {
-        colorFiller.fillCell(drawable, cell, color)
+    colorFiller.fillCell(drawable, cell, color)
   }
 
   private def fillWhiteBackground(drawable: IDrawable): Unit = {
@@ -185,9 +188,7 @@ class MainViewController extends ICellStateObserver {
 
   override def onCellsStateChange(cells: Array[Array[Cell]]): Unit = {
     Platform.runLater(() => {
-      println("a")
       cells.foreach(_.foreach(onCellStateChange))
-      println("b")
     })
   }
 
