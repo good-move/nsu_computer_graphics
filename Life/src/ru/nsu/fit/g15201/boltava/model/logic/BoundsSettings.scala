45eb17112b856ae0d525235b187aa5fb393413a3
Alexey Boltava <goodmoveab@gmail.com>
2018-02-26 01:50:59 +0700

    Add BoundsSettings helper class

---
+++ Life/src/ru/nsu/fit/g15201/boltava/model/logic/BoundsSettings.scala
@@ -0,0 +1,11 @@
+package ru.nsu.fit.g15201.boltava.model.logic
+
+class BoundsSettings {
+
+  var maxCellBorderSize: Int = _
+  var minCellBorderSize: Int = _
+
+  var minCellBorderWidth: Int = _
+  var maxCellBorderWidth: Int = _
+
+}
