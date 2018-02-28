5bc52df0ebbf6c173f84e422646d36a4a8245e29
Alexey Boltava <goodmoveab@gmail.com>
2018-02-28 23:51:35 +0700

    Refactor ToolbarInteractor

--- Life/src/ru/nsu/fit/g15201/boltava/presentation_layer/toolbar/ToolbarInteractor.scala
+++ Life/src/ru/nsu/fit/g15201/boltava/presentation_layer/toolbar/ToolbarInteractor.scala
@@ -3,13 +3,14 @@ package ru.nsu.fit.g15201.boltava.presentation_layer.toolbar
 import ru.nsu.fit.g15201.boltava.domain_layer.canvas.HexagonalGridController
 import ru.nsu.fit.g15201.boltava.domain_layer.canvas.geometry.Point
 import ru.nsu.fit.g15201.boltava.domain_layer.logic._
+import ru.nsu.fit.g15201.boltava.domain_layer.logic.settings.ISettingsController
 import ru.nsu.fit.g15201.boltava.presentation_layer.main.MainActivity
 import ru.nsu.fit.g15201.boltava.presentation_layer.settings.SettingsPaneActivity
 import ru.nsu.fit.g15201.boltava.presentation_layer.toolbar.IContract.IInteractor
 
 import scala.collection.mutable.ArrayBuffer
 
-class ToolbarInteractor(private val gameController: IGameLogicController) extends IInteractor {
+class ToolbarInteractor(private val gameController: IGameLogicController, private val settingsController: ISettingsController) extends IInteractor {
 
   override def onSaveModel(path: String): Unit = {
     val aliveCells = new ArrayBuffer[Point]()
@@ -18,13 +19,14 @@ class ToolbarInteractor(private val gameController: IGameLogicController) extend
         aliveCells.append((cell.getX, cell.getY))
       }
     }))
-    ConfigManager.saveGameModel(path, gameController.getGameSettings, aliveCells.toArray)
+    ConfigManager.saveGameModel(path, gameController.getGameSettings.playgroundSettings, aliveCells.toArray)
   }
 
   override def onOpenModel(path: String): Unit = {
     val gridParameters = ConfigManager.openGameModel(path)
     gameController.setGridController(new HexagonalGridController(gridParameters.borderSize))
-    gameController.setGridParams(gridParameters)
+    gameController.setPlaygroundSettings(gridParameters)
+    gameController.initGame()
   }
 
   override def onSetReplace(): Unit = {
@@ -37,7 +39,7 @@ class ToolbarInteractor(private val gameController: IGameLogicController) extend
 
   override def onOpenSettings(): Unit = {
     // todo: fix weird stuff (view inside model)!
-    SettingsPaneActivity.launch(MainActivity.getWindow, gameController)
+    SettingsPaneActivity.launch(MainActivity.getWindow, settingsController)
   }
 
   override def getGameController: IGameLogicController = gameController
