1e11f4f29fa4ddc60914657382f64362dbcea514
Alexey Boltava <goodmoveab@gmail.com>
2018-02-28 23:53:02 +0700

    Simplify SettingsPaneActivity

--- Life/src/ru/nsu/fit/g15201/boltava/presentation_layer/settings/SettingsPaneActivity.scala
+++ Life/src/ru/nsu/fit/g15201/boltava/presentation_layer/settings/SettingsPaneActivity.scala
@@ -2,36 +2,20 @@ package ru.nsu.fit.g15201.boltava.presentation_layer.settings
 
 import javafx.stage.Window
 
-import ru.nsu.fit.g15201.boltava.domain_layer.logic.{BoundsSettings, GameSettings, IGameLogicController}
-import ru.nsu.fit.g15201.boltava.presentation_layer.settings.IContract.{IInteractor, IPresenter}
+import ru.nsu.fit.g15201.boltava.domain_layer.logic.settings._
+import ru.nsu.fit.g15201.boltava.presentation_layer.settings.IContract.IPresenter
 
 object SettingsPaneActivity {
 
   private var settingsPane: SettingsPane = _
-  private var gameController: IGameLogicController = _
+  private var gameController: ISettingsController = _
   private var presenter: IPresenter = _
 
-  def launch(owner: Window, gameController: IGameLogicController): Unit = {
-    this.gameController = gameController
+  def launch(owner: Window, settingsController: ISettingsController): Unit = {
+    this.gameController = settingsController
     settingsPane = new SettingsPane(owner)
-    presenter = new SettingsPanePresenter(settingsPane.getController, new SettingsPaneActivity)
+    presenter = new SettingsPanePresenter(settingsPane.getController, new SettingsPaneInteractor(settingsController))
     settingsPane.show()
-  }
-
-}
-
-class SettingsPaneActivity extends IInteractor {
-
-  override def getGameSettings: GameSettings = {
-    SettingsPaneActivity.gameController.getGameSettings
-  }
-
-  override def getBoundsSettings: BoundsSettings = {
-    SettingsPaneActivity.gameController.getBoundsSettings
-  }
-
-  override def applyGameSettings(gameSettings: GameSettings): Unit = {
-//    SettingsPaneActivity.gameController.applyGameSettings(gameSettings)
   }
 
 }
