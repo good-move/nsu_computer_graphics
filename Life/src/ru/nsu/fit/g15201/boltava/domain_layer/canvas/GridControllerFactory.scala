4f9b2c64646441c37453e2e01f399cf49f608d27
Alexey Boltava <goodmoveab@gmail.com>
2018-03-05 21:46:41 +0700

    Create grid controller factory

---
+++ Life/src/ru/nsu/fit/g15201/boltava/domain_layer/canvas/GridControllerFactory.scala
@@ -0,0 +1,21 @@
+package ru.nsu.fit.g15201.boltava.domain_layer.canvas
+
+import ru.nsu.fit.g15201.boltava.domain_layer.logic.GridType.GridType
+import ru.nsu.fit.g15201.boltava.domain_layer.logic.GridType._
+
+
+object GridControllerFactory {
+
+  def apply(gridType: GridType)(cellSideSize: Int): IGridController = {
+
+    gridType match {
+      case HEXAGON => new HexagonalGridController(cellSideSize)
+      case SQUARE => new SquareGridController(cellSideSize)
+      case _ => throw UnknownGridTypeException(s"Unknown grid type: $gridType")
+    }
+
+  }
+}
+
+
+case class UnknownGridTypeException(message: String) extends Exception(message)
---
+++ Life/src/ru/nsu/fit/g15201/boltava/domain_layer/logic/GridType.scala
@@ -0,0 +1,6 @@
+package ru.nsu.fit.g15201.boltava.domain_layer.logic
+
+object GridType extends Enumeration {
+  type GridType = Value
+  val HEXAGON, SQUARE, TRIANGLE = Value
+}
