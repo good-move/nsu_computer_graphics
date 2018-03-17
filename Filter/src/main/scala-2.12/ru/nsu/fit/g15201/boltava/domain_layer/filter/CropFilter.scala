5788e2c8446a33c18c2cb48fcef33527c14a7fc9
Alexey Boltava <goodmoveab@gmail.com>
2018-03-17 22:21:12 +0700

    Add CropFilter

---
+++ Filter/src/main/scala-2.12/ru/nsu/fit/g15201/boltava/domain_layer/filter/CropFilter.scala
@@ -0,0 +1,46 @@
+package ru.nsu.fit.g15201.boltava.domain_layer.filter
+
+import ru.nsu.fit.g15201.boltava.domain_layer.geometry.IntPoint
+
+object CropFilter {
+
+  def apply(topLeft: IntPoint, bottomRight: IntPoint): CropFilter = new CropFilter(topLeft, bottomRight)
+  def apply(topLeft: IntPoint, width: Int, height: Int): CropFilter = {
+    if (width <= 0 || height <= 0) {
+      throw new IllegalArgumentException("Width and Height of crop rectangle must be positive")
+    }
+    new CropFilter(topLeft, IntPoint(topLeft.x + width, topLeft.y + height))
+  }
+
+}
+
+class CropFilter(private val topLeft: IntPoint,
+                 private val bottomRight: IntPoint
+                ) extends Transformer {
+
+  override def transform(image: RawImage): RawImage = {
+    if (isPointInsideImage(image, topLeft) && isPointInsideImage(image, bottomRight)) {
+      _transform(image)
+    } else {
+      throw new IllegalStateException("Crop rectangle is outside image dimensions")
+    }
+  }
+
+  private def isPointInsideImage(image: Image, point: IntPoint): Boolean = {
+    (0 <= point.x && point.x <= image.width) && (0 <= point.y && point.y <= image.height)
+  }
+
+  private def _transform(image: RawImage): RawImage = {
+    val content = for {
+      row <- topLeft.y until bottomRight.y
+      col <- topLeft.x until bottomRight.x
+    } yield image.content(row * image.width + col)
+
+    RawImage(
+      bottomRight.x - topLeft.x,
+      bottomRight.y - topLeft.y,
+      content.toArray
+    )
+  }
+
+}
