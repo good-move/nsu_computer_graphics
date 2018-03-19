afcf046c93acd309b1b0e27f665edb7072e9d6b5
Alexey Boltava <goodmoveab@gmail.com>
2018-03-19 14:35:43 +0700

    Update MainActivity to combine and inject all view components

--- Filter/src/main/scala-2.12/ru/nsu/fit/g15201/boltava/presentation_layer/main_activity/MainActivity.scala
+++ Filter/src/main/scala-2.12/ru/nsu/fit/g15201/boltava/presentation_layer/main_activity/MainActivity.scala
@@ -1,46 +1,41 @@
 package ru.nsu.fit.g15201.boltava.presentation_layer.main_activity
 
 import ru.nsu.fit.g15201.boltava.presentation_layer.extension.IBaseActivity
+import ru.nsu.fit.g15201.boltava.presentation_layer.menu.{MenuComponent, MenuInteractor}
 import ru.nsu.fit.g15201.boltava.presentation_layer.toolbar.Contract.IToolbarPresenter
-import ru.nsu.fit.g15201.boltava.presentation_layer.toolbar.{ToolbarComponent, ToolbarInteractor, ToolbarPresenter}
+import ru.nsu.fit.g15201.boltava.presentation_layer.toolbar.{ToolbarComponent, ToolbarPresenter}
 import ru.nsu.fit.g15201.boltava.presentation_layer.workbench.Contract.IWorkbenchPresenter
 import ru.nsu.fit.g15201.boltava.presentation_layer.workbench.{WorkbenchComponent, WorkbenchPresenter}
-
 import scalafx.scene.Scene
 import scalafx.scene.layout.{Priority, VBox}
 import scalafx.stage.{Stage, Window}
 
 object MainActivity extends IBaseActivity[MainContext] {
 
-  private var window: Option[Window] = None
   private var workbenchPresenter: Option[IWorkbenchPresenter] = None
   private var toolbarPresenter: Option[IToolbarPresenter] = None
 
   override def launch(context: Option[MainContext] = None)(implicit stage: Stage): Unit = {
-    window = Some(stage)
+
+    val menuInteractor = new MenuInteractor
 
     val workbench = WorkbenchComponent()
     val toolbar = ToolbarComponent()
+    val menu = MenuComponent(menuInteractor)
 
     workbenchPresenter = Some(new WorkbenchPresenter(workbench.view))
-    toolbarPresenter = Some(new ToolbarPresenter(toolbar.view, new ToolbarInteractor))
+    toolbarPresenter = Some(new ToolbarPresenter(toolbar.view, menu.presenter, menuInteractor))
 
     stage.scene = new Scene {
       root = new VBox {
         children = Seq(
+          menu.root,
           toolbar.root,
           workbench.root
         )
         workbench.root.vgrow = Priority.Always
       }
     }
-  }
-
-  override def getWindow: Window = {
-    if (window.isEmpty) {
-      throw UninitializedFieldError("Window is unavailable")
-    }
-    window.get
   }
 
 }
