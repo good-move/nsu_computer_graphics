dbb5f5eed22773f9e46260b2795f1c1566ee63f0
Alexey Boltava <goodmoveab@gmail.com>
2018-04-04 19:41:18 +0700

    Add class FieldParameters

---
+++ Isolines/src/main/scala/ru/nsu/fit/g15201/boltava/domain_layer/logic/settings/FieldParameters.scala
@@ -0,0 +1,9 @@
+package ru.nsu.fit.g15201.boltava.domain_layer.logic.settings
+
+case class FieldParameters(xNodes: Int,
+                           yNodes: Int,
+                           lowerXBound: Double,
+                           upperXBound: Double,
+                           lowerYBound: Double,
+                           upperYBound: Double
+                          )
